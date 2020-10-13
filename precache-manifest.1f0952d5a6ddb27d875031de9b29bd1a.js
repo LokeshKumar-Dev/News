46c3fed8ae04e2e0ed351fac2f983eafd307f188
lokesh <adobep40@gmail.com>
2020-10-12 22:01:00 -0700

    Updates

---
+++ precache-manifest.1f0952d5a6ddb27d875031de9b29bd1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09424a1878270d229d65814f48c4cdb",
+    "url": "/News/index.html"
+  },
+  {
+    "revision": "2d26d2816e51da1f8b5d",
+    "url": "/News/static/js/2.a9387deb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/News/static/js/2.a9387deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b2bcbfb66728fe5131",
+    "url": "/News/static/js/main.4ceaafb1.chunk.js"
+  },
+  {
+    "revision": "e1f715b0713bfc175088",
+    "url": "/News/static/js/runtime-main.a8922881.js"
+  }
+]);
